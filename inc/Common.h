f1dd7a1c0871a845f75ddd39b30a3040c84ed517
IndianPrince <harryhatchet@yahoo.com>
2025-04-17 14:39:09 +0300

    hw1

--- inc/Common.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <array>
-#include <iostream>
-#include <string>
-
-#define _USE_MATH_DEFINES
-#include <math.h>
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <common.h>
-
-
-
-int main()
-{
-}
-
